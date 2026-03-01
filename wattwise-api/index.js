05c4a8f2dbc820ce2934f1a368028f8bde20da53
anushkaa0507 <anushkaramrakhyaa@gmail.com>
2026-03-01 20:31:05 +0530

    testing deployment

--- wattwise-api/index.js
+++ wattwise-api/index.js
@@ -92,3 +92,6 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () =>
   console.log(`⚡ Wattwise API running on port ${PORT}`)
 );
+app.get("/", (req, res) => {
+  res.send("Wattwise API Running 🚀");
+});
